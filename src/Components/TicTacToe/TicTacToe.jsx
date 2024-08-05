0bd56ab01d7c9ef004f4cb774488f6a93d4144d0
Afolabi Abeeb Ademola <aphoo4real@gmail.com>
2024-08-05 02:12:05 +0100

    added files

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -6,7 +6,22 @@ const TicTacToe = () => {
     <div className='container'>
       <h1 className="title">Tic Tac Toe Game By <span>Aphoo</span></h1>
       <div className="board">
-
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      
       </div>
       <button className="reset">Reset</button>
     </div>
